173a45e64658d2a0efb68644eca35c3cece0f8dd
AfiaTasnim <tasnimafia7860@gmail.com>
2026-03-18 22:18:32 +0600

    booking routes for vaccination campaigns

--- server/src/routes/vaccinationCampaignRoutes.js
+++ server/src/routes/vaccinationCampaignRoutes.js
@@ -3,12 +3,18 @@ const {
   listCampaigns,
   createCampaign,
   updateCampaign,
-  deleteCampaign
+  deleteCampaign,
+  bookCampaignAppointment,
+  listMyBookings
 } = require("../controllers/vaccinationCampaignController");
 const { requireAuth, requireRole } = require("../middlewares/auth");
 
 // Public route
 router.get("/", listCampaigns);
+
+// User
+router.get("/my-bookings", requireAuth, listMyBookings);
+router.post("/:id/book", requireAuth, bookCampaignAppointment);
 
 // Admin-only
 router.post("/", requireAuth, requireRole("admin"), createCampaign);
